c906f2e31faac5d0542758dd30bcfc645fd4bc96
Jibbril Berbres <jibbril.berbres@viego.se>
2023-05-30 22:27:52 +0200

    Setup suggestion row example

--- app/inputsuggestions/webapp/lib/actions.js
+++ app/inputsuggestions/webapp/lib/actions.js
@@ -1,10 +1,38 @@
 sap.ui.define([
-
-], function () {
+    "sap/m/Dialog",
+    "sap/m/Input",
+    "sap/m/Column",
+    "sap/m/ColumnListItem",
+    "sap/m/Label",
+], function (Dialog, Input, Column, ColumnListItem, Label) {
     return   {
         edit: function (oEvent, aContexts) {
-            debugger
-            console.log("EDITTTTTT")
+            const oDialog = new Dialog({
+                title: 'Edit Dialog',
+                content: [
+                    new Input({
+                        showSuggestion: true,
+                        showTableSuggestionValueHelp: false,
+                        showValueHelp: true,
+                        suggestionRows: {
+                            path: '/Authors',
+                            template: new ColumnListItem({
+                                cells: [
+                                    new Label({ text: '{name}' })
+                                ]
+                            }) 
+                        },
+                        suggestionColumns: [
+                            new Column({
+                                header: new Label({ text: 'Name' })
+                            })
+                        ]
+                    })
+                ],
+            });
+
+            oDialog.setModel(this.getModel());
+            oDialog.open();
         }
     }
 })
