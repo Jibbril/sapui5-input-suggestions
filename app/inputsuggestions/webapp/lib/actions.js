458e81dbeba0a85c1c6eaf5d0e6107358c17749f
Jibbril Berbres <jibbril.berbres@viego.se>
2023-05-31 15:04:39 +0200

    Enable value help popover

--- app/inputsuggestions/webapp/lib/actions.js
+++ app/inputsuggestions/webapp/lib/actions.js
@@ -1,12 +1,34 @@
 sap.ui.define([
+    "sap/ui/model/json/JSONModel",
     "sap/m/Dialog",
     "sap/m/Input",
     "sap/m/Column",
     "sap/m/ColumnListItem",
     "sap/m/Label",
-], function (Dialog, Input, Column, ColumnListItem, Label) {
+    "sap/m/TableSelectDialog",
+    'sap/ui/model/Filter',
+	'sap/ui/model/FilterOperator',
+], function (JSONModel, Dialog, Input, Column, ColumnListItem, Label, TableSelectDialog, Filter, FilterOperator) {
     return   {
         edit: function (oEvent, aContexts) {
+            const oDataModel = this.getModel(); 
+            const getItems = (text) => {
+                return {
+                    path: '/Authors',
+                    template: new ColumnListItem({
+                        cells: [
+                            new Label({ text })
+                        ]
+                    })
+                }
+            }
+            
+            const columns = [
+                new Column({
+                    header: new Label({ text: 'Name' })
+                })
+            ]
+
             const oDialog = new Dialog({
                 title: 'Edit Dialog',
                 content: [
@@ -14,24 +36,50 @@ sap.ui.define([
                         showSuggestion: true,
                         showTableSuggestionValueHelp: false,
                         showValueHelp: true,
-                        suggestionRows: {
-                            path: '/Authors',
-                            template: new ColumnListItem({
-                                cells: [
-                                    new Label({ text: '{name}' })
-                                ]
-                            }) 
-                        },
-                        suggestionColumns: [
-                            new Column({
-                                header: new Label({ text: 'Name' })
+                        suggestionRows: getItems('{name}'),
+                        suggestionColumns: columns,
+                        valueHelpRequest: (event) => {
+                            const dialog = new TableSelectDialog({
+                                title: 'Authors',
+                                items: getItems('{name}'),
+                                columns,
                             })
-                        ]
+
+                            const handleSearch = (event) => {
+                                const value = event.getParameter('value');
+                                const filters = [];
+                                if (value) {
+                                    filters.push(new Filter({
+                                        filters: [
+                                            new Filter({ path: 'name', operator: FilterOperator.Contains, value1: value }),
+                                        ]
+                                    }));
+                                }
+                                dialog.getBinding('items').filter(filters);
+                            }
+
+                            dialog.attachSearch(handleSearch);
+                            dialog.attachLiveChange(handleSearch);
+
+                            dialog.setModel(oDataModel);
+                            // dialog.bindAggregation('items', getItems('{name}'));
+                            dialog.open(); 
+                        }
                     })
                 ],
             });
 
-            oDialog.setModel(this.getModel());
+            oDialog.setModel(oDataModel);
+
+            const jsonModel = new JSONModel({
+                Authors: [
+                    { Name: 'Author 1' },
+                    { Name: 'Author 2' },
+                    { Name: 'Author 3' },
+                    { Name: 'Author 4' },
+                ]
+            }); 
+            oDialog.setModel(jsonModel, 'dialog');
             oDialog.open();
         }
     }
